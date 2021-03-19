5468ba3d1c64b7484d54e0c4eb6c82997f8cb69b
jep1220 <38770396+jep1220@users.noreply.github.com>
2021-03-18 17:48:33 -0700

    routes added server.js

--- server.js
+++ server.js
@@ -44,3 +44,50 @@ var sessionChecker = (req, res, next) => {
             next();
         }
 };
+
+//route for home page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+// route for user signup
+app.route('/signup')
+    .get((req, res) => {
+        res.render('signup', hbsContent);
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            password: req.body.password
+        })
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
+
+// route for user login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.render('login', hbsContent);
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+    
