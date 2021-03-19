05ba8c5cdf062ec6bc81bbab19c181cbe9126544
jep1220 <38770396+jep1220@users.noreply.github.com>
2021-03-18 17:37:07 -0700

    login and middleware

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var morgan = require('morgan');
 var User = require('./models/user');
 var hbs = require('express-handlebars'); 
 var path = require('path'); 
+const { nextTick } = require('node:process');
 
+// invoke an instance of express application.
 var app = express();
 app.set('port', 9000);
 app.use(morgan('dev'));
@@ -21,3 +23,24 @@ app.use(session({
             expires: 600000
         }
 }));
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/view/layouts'}));
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user){
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+
+var hbsContent = {userName: '', loggedin: false, title: "You are not logged in", body: "hello"};
+
+//middle ware function to check for logged in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookes.user_sid) {
+        res.redirect('/dashboard');
+        } else {
+            next();
+        }
+};
